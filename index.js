6a0584145e897cffca57d9a57dd940728158b536
fatemaYellow <96525651+fatemaYellow@users.noreply.github.com>
2024-12-24 17:55:00 +0600

    one more api end point add

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.listen(port, () => {
 
 const options = {
     method: 'GET',
-    url: 'https://wise.com/rates/history+live',
+    url: '	https://api.wise.com/v1/rates?source=GBP&target=USD',
     params: {
       source: 'USD',
       target: 'GBP',
@@ -40,9 +40,24 @@ const options = {
       'Priority': 'u=0'
     }
   };
+  const url = 'https://api.wise.com/v1/rates?source=GBP&target=USD';
+  const headers = {
+    'Authorization': 'Basic OGNhN2FlMjUtOTNjNS00MmFlLThhYjQtMzlkZTFlOTQzZDEwOjliN2UzNmZkLWRjYjgtNDEwZS1hYzc3LTQ5NGRmYmEyZGJjZA=='
+  };
+  
+  // Your endpoint to fetch currency exchange rate
+app.get('/get-current', async (req, res) => {
+  try {
+      const response = await axios.get(url, { headers });
+      res.json(response.data);
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Error fetching data');
+  }
+});
 
 // Your endpoint to fetch currency exchange rate
-app.get('/get-rate', async (req, res) => {
+app.get('/history-live', async (req, res) => {
     try {
         const response = await axios.get('https://wise.com/rates/history+live?source=USD&target=GBP&length=2&resolution=hourly&unit=day');
         res.json(response.data);
